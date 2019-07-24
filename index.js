a3c38000c5a919db57fa7e2079b3eccc564faf8b
Alexis Colman <alexis.colman@arbusta.net>
2019-07-24 15:20:26 -0300

    Se agregó un comentario

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-        const name = process.argv.lenght > 2 ? process.argv[2] : "arbusta"
+//yolo
+const name = process.argv.lenght > 2 ? process.argv[2] : "arbusta"
 
         console.log("hola ${name}")
